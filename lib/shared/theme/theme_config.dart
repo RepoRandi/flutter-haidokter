8a900ef977a85674d1fb06c06f897e9d706f5edb
Randi <randimaulanaakbar@gmail.com>
2023-10-23 07:38:07 +0700

    feat: add theme config

--- lib/shared/theme/theme_config.dart
+++ lib/shared/theme/theme_config.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 var primaryColor = Color(0xff321fdb);
 var secondaryColor = Color(0xffef9920);
 var disabledColor = Color(0xffcdcdcd);
-var textColor = Color(0xff010F07);
+var textColor = Colors.white70;
 var secondaryTextColor = Color(0xff868686);
 var scaffoldBackgroundColor = Color(0xFFFFFF);
 
 var successColor = Color(0xff2eb85c);
-var infoColor = Color(0xff3399ff);
+var infoColor = Color(0xff1878f3);
 var warningColor = Color(0xfff9b115);
-var dangerColor = Color(0xffe55353);
+var dangerColor = Color(0xffc71610);
 
 var blueColor = Color(0xff006dfa);
+var lightColor = Color(0xff6c96de);
 var darkBlueColor = Color(0xff003e88);
