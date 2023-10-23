6f2b84d840e8cdb1d96d0098105ae8c9f13010cb
Randi <randimaulanaakbar@gmail.com>
2023-10-23 10:36:17 +0700

    feat: add widget patient dashboard search

---
+++ lib/module/patient/patient_dashboard/widget/patient_dashboard_search.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PatientDashboardSearch extends StatelessWidget {
+  const PatientDashboardSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 5.0,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search',
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(
+              color: Colors.grey[300]!,
+            ),
+          ),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.blueGrey[900],
+          ),
+          suffixIcon: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.sort,
+              color: Colors.blueGrey[900],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
