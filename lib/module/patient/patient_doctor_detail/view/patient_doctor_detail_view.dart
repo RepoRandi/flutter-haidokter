701ab009d7c48fc8c281970944b06fe2731730f1
Randi <randimaulanaakbar@gmail.com>
2023-10-26 20:47:37 +0700

    feat: add patient doctor detail view

--- lib/module/patient/patient_doctor_detail/view/patient_doctor_detail_view.dart
+++ lib/module/patient/patient_doctor_detail/view/patient_doctor_detail_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../bloc/patient_doctor_detail_bloc.dart';
-import '../state/patient_doctor_detail_state.dart';
+import 'package:haidokter/core.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/doctor_card.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/doctor_schedule_card.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/expandable_doctor_card.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/expandable_educational_background.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/expandable_medical_treatment_card.dart';
+import 'package:haidokter/module/patient/patient_doctor_detail/widget/expandable_pratical_experience_card.dart';
 
 class PatientDoctorDetailView extends StatefulWidget {
   final Map doctor;
@@ -67,10 +72,94 @@ class _PatientDoctorDetailViewState extends State<PatientDoctorDetailView> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [],
+      body: SingleChildScrollView(
+        controller: ScrollController(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            DoctorCard(
+              photoUrl: '${widget.doctor['photo']}',
+              doctorName: '${widget.doctor['doctor_name']}',
+              specialization: '${widget.doctor['specialization']}',
+              patientCount: '${widget.doctor['patient_count']} pasien ',
+              onPressed: () {},
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: DoctorScheduleCard(
+                title: 'Lokasi & Jadwal Praktik Terdekat',
+                hospitalPhotoUrl: '${widget.doctor['hospital_photo']}',
+                hospitalName: '${widget.doctor['hospital']}',
+                address: '${widget.doctor['address']}',
+                distanceInKm: '${widget.doctor['location_in_km']}',
+                consultationFee: '${widget.doctor['price']}',
+                schedules: [
+                  'Senin, 1 Oktober 2023',
+                  'Selasa, 2 Oktober 2023',
+                  'Rabu, 3 Oktober 2023'
+                ],
+                onScheduleTapped: () {},
+                onSeeAllTapped: () {},
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: ExpandableDoctorCard(
+                title: 'Profil Dokter',
+                collapsedText:
+                    '${widget.doctor['doctor_name']} adalah Dokter Spesialis ${widget.doctor['specialization']} yang berpraktek di ${widget.doctor['hospital']}',
+                expandedText:
+                    '${widget.doctor['doctor_name']} adalah Dokter Spesialis ${widget.doctor['specialization']} yang berpraktek di ${widget.doctor['hospital']}',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: ExpandableMedicalTreatmentCard(
+                title: 'Tindakan Medis',
+                items: [
+                  'Fluoride Treatment',
+                  'Foto Panoramic Gigi',
+                  'Pembersihan Karang Gigi',
+                  'Pemeriksaan Gusi',
+                  'Perawatan Saluran Akar',
+                  'Pemasangan Behel',
+                  'Pemutihan Gigi',
+                  'Cabut Gigi',
+                  'Pemeriksaan Rontgen Gigi',
+                  'Perawatan Gigi Palsu',
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: ExpandablePraticalExperienceCard(
+                title: 'Pengalaman Praktik',
+                content:
+                    '${widget.doctor['specialization']} - ${widget.doctor['hospital']}',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 16, right: 16),
+              child: ExpandableEducationalBackgroundCard(
+                title: 'Riwayat Pendidikan',
+                content:
+                    'Sp.KG - Spesialis Konservasi Gigi - Universitas Brawijaya',
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 90,
+        padding: EdgeInsets.all(20),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: orangeColor,
+          ),
+          child: const Text('Buat Janji Konsultasi'),
+          onPressed: () {},
+        ),
       ),
     );
   }
