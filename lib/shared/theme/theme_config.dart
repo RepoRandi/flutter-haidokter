0acc3f1d10deefeff8898574f7544309f8da4a2b
Randi <randimaulanaakbar@gmail.com>
2023-10-28 16:05:48 +0700

    feat: add color

--- lib/shared/theme/theme_config.dart
+++ lib/shared/theme/theme_config.dart
@@ -17,4 +17,6 @@ var lightColor = Color(0xff6c96de);
 var darkBlueColor = Color(0xff003e88);
 var pinkColor = Color(0xffee5693);
 var greyColor = Color(0xff7f7f7f);
+var lightGreyColor = Color(0xffe9e9e9);
 var orangeColor = Color(0xfffa6d01);
+var redColor = Color(0xfff7586a);
